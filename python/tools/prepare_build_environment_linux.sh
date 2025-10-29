952671fea5a7b670f65a855e546e43c4b4f21efe
Jordi Mas <jmas@softcatala.org>
2025-10-29 11:00:31 +0100

    fix dep

--- python/tools/prepare_build_environment_linux.sh
+++ python/tools/prepare_build_environment_linux.sh
@@ -32,6 +32,7 @@ else
         libcurand-devel-12-2-10.3.3.141-1 \
         libcudnn9-devel-cuda-12-9.1.0.70-1 \
         libcublas-devel-12-2-12.2.5.6-1 \
+        libnccl-2.19.3-1+cuda12.2 \
         libnccl-devel-2.19.3-1+cuda12.2
     ln -s cuda-12.2 /usr/local/cuda
 

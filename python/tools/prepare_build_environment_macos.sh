195f5aea4a0e85e79983da64f0bb75109ac70201
Jordi Mas <jmas@softcatala.org>
2025-10-30 20:44:16 +0100

    ruy off

--- python/tools/prepare_build_environment_macos.sh
+++ python/tools/prepare_build_environment_macos.sh
@@ -1,35 +1,23 @@
-#!/bin/bash
+#! /bin/bash
+
 set -e
 set -x
 
-# Ensure CMake and LLVM setup
 pip install "cmake==3.18.4"
-cmake --version
-
-brew install llvm libomp
-
-# Detect correct LLVM prefix dynamically
-LLVM_PREFIX=$(brew --prefix llvm)
-
-export PATH="$LLVM_PREFIX/bin:$PATH"
-export CC="$LLVM_PREFIX/bin/clang"
-export CXX="$LLVM_PREFIX/bin/clang++"
-export LDFLAGS="-L$LLVM_PREFIX/lib -L$(brew --prefix libomp)/lib"
-export CPPFLAGS="-I$LLVM_PREFIX/include -I$(brew --prefix libomp)/include"
-export CFLAGS="$CPPFLAGS"
-export CXXFLAGS="$CPPFLAGS -fopenmp"
-export CMAKE_CXX_FLAGS="-fopenmp"
-export CMAKE_EXE_LINKER_FLAGS="-L$(brew --prefix libomp)/lib -lomp"
 
 mkdir build-release && cd build-release
 
 CMAKE_EXTRA_OPTIONS=''
 
 if [ "$CIBW_ARCHS" == "arm64" ]; then
+
     CMAKE_EXTRA_OPTIONS='-DCMAKE_OSX_ARCHITECTURES=arm64 -DWITH_ACCELERATE=ON -DWITH_MKL=OFF -DOPENMP_RUNTIME=NONE -DWITH_RUY=OFF'
+
 else
-    # Install Intel oneAPI MKL
-    ONEAPI_INSTALLER_URL="https://registrationcenter-download.intel.com/akdlm/IRC_NAS/cd013e6c-49c4-488b-8b86-25df6693a9b7/m_BaseKit_p_2023.2.0.49398.dmg"
+
+    # Install OneAPI MKL
+    # See https://github.com/oneapi-src/oneapi-ci for installer URLs
+    ONEAPI_INSTALLER_URL=https://registrationcenter-download.intel.com/akdlm/IRC_NAS/cd013e6c-49c4-488b-8b86-25df6693a9b7/m_BaseKit_p_2023.2.0.49398.dmg
     wget -q $ONEAPI_INSTALLER_URL
     hdiutil attach -noverify -noautofsck $(basename $ONEAPI_INSTALLER_URL)
     sudo /Volumes/$(basename $ONEAPI_INSTALLER_URL .dmg)/bootstrapper.app/Contents/MacOS/bootstrapper --silent --eula accept --components intel.oneapi.mac.mkl.devel
@@ -38,31 +26,18 @@ else
     wget -q https://github.com/oneapi-src/oneDNN/archive/refs/tags/v${ONEDNN_VERSION}.tar.gz
     tar xf *.tar.gz && rm *.tar.gz
     cd oneDNN-*
-    cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-          -DCMAKE_BUILD_TYPE=Release \
-          -DONEDNN_LIBRARY_TYPE=STATIC \
-          -DONEDNN_BUILD_EXAMPLES=OFF \
-          -DONEDNN_BUILD_TESTS=OFF \
-          -DONEDNN_ENABLE_WORKLOAD=INFERENCE \
-          -DONEDNN_ENABLE_PRIMITIVE="CONVOLUTION;REORDER" \
-          -DONEDNN_BUILD_GRAPH=OFF .
+    cmake -DCMAKE_BUILD_TYPE=Release -DONEDNN_LIBRARY_TYPE=STATIC -DONEDNN_BUILD_EXAMPLES=OFF -DONEDNN_BUILD_TESTS=OFF -DONEDNN_ENABLE_WORKLOAD=INFERENCE -DONEDNN_ENABLE_PRIMITIVE="CONVOLUTION;REORDER" -DONEDNN_BUILD_GRAPH=OFF .
     make -j$(sysctl -n hw.physicalcpu_max) install
     cd ..
     rm -r oneDNN-*
+
     CMAKE_EXTRA_OPTIONS='-DWITH_DNNL=ON'
+
 fi
 
-# Re-run cmake and build
-cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_CLI=OFF \
-      -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
-      $CMAKE_EXTRA_OPTIONS \
-      ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_CLI=OFF -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON $CMAKE_EXTRA_OPTIONS ..
 VERBOSE=1 make -j$(sysctl -n hw.physicalcpu_max) install
-
 cd ..
 rm -r build-release
 
 cp README.md python/
-

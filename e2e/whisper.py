ec89743b99b3eaed75d6bc99af62a7dfa4dcbe58
Jordi Mas <jmas@softcatala.org>
2025-11-08 18:49:32 +0100

    Simply

---
+++ e2e/whisper.py
@@ -0,0 +1,39 @@
+import time
+import ctranslate2
+import librosa
+import transformers
+
+# --- Load audio ---
+audio, sr = librosa.load("e2e/dosparlants.mp3", sr=16000, mono=True)
+audio_duration = len(audio) / sr
+
+# --- Initialize processor & model ---
+processor = transformers.WhisperProcessor.from_pretrained("openai/whisper-tiny")
+model = ctranslate2.models.Whisper("whisper-tiny-ct2")
+
+# --- Feature extraction ---
+start_feat = time.time()
+features = ctranslate2.StorageView.from_array(
+    processor(audio, return_tensors="np", sampling_rate=sr).input_features
+)
+# --- Language detection ---
+language = model.detect_language(features)[0][0][0]
+print(f"Detected language: {language}")
+
+# --- Prepare prompt & transcription ---
+prompt = processor.tokenizer.convert_tokens_to_ids(
+    ["<|startoftranscript|>", language, "<|transcribe|>", "<|notimestamps|>"]
+)
+start_gen = time.time()
+seq_ids = model.generate(features, [prompt])[0].sequences_ids[0]
+gen_duration = time.time() - start_gen
+
+# --- Decode transcription ---
+transcription = processor.decode(seq_ids)
+print(f"\nTranscription:\n{transcription}\n")
+
+# --- Performance ---
+num_tokens = len(seq_ids)
+rtf = gen_duration / audio_duration
+print(f"Generated {num_tokens} tokens in {gen_duration:.2f} sec | RTF: {rtf:.2f}")
+
